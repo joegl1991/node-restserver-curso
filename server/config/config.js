b246279be10aa0f17ed5933916c0fcd120e2daad
Hector Garcia <joegl1991@gmail.com>
2020-07-02 17:38:14 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://strider:yUPar954cMqRCecE@cluster0.lboon.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
